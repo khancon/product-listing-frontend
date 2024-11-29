baaafcf2d8fa52ba37ea96901881a3a13b1ee825
Ahnaf Khan <akhan356@gatech.edu>
2024-11-29 18:00:03 -0500

    final changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,4 +5,12 @@ import BASE_URL from './config';
 
 const App = () => {
   return (
-    <div style={{ paddin
+    <div style={{ padding: '16px' }}>
+      <ProductList />
+      <p>Base URL -- {BASE_URL}</p>
+      <p>ENV -- {process.env.REACT_APP_ENV || "REACT_APP_ENV not found"}</p>
+    </div>
+  );
+}
+
+export default App;
